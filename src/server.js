98276407b56271792483e4d27a42d2bb742524c7
foisal <foisalrk2@gmail.com>
2024-10-18 05:11:57 +0600

    sdfasdfas

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 import { app } from "./app.js";
 
-
-
+//HM5ZYrGZmY9rXWhO
+//car-rental-service
 // ekhane server rekheci
 let server;
 const port = 3000
 
 const main = async()=>{
   try {
-    await mongoose.connect("mongodb://localhost:27017/CarWebsite")
+    await mongoose.connect("mongodb+srv://car-rental-service:HM5ZYrGZmY9rXWhO@cluster0.xlk7a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    
     console.log("connected")
     server = app.listen(port, () => {
       console.log(`Server started on port ${port}`);
